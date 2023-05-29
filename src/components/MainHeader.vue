52b135c4d86a3d9d6359d6f48df011ed7e91d52a
Shafi Akinropo <sakinropo@gmail.com>
2023-05-29 20:33:42 +0100

    update

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -2,7 +2,7 @@
 <template>
    <div class="bg-dark">
       <div class="line-pattern"></div>
-      <nav class="navbar navbar-expand-lg bg-transparent">
+      <nav class="navbar navbar-expand-lg bg-light">
         <div class="container">
           <a class="navbar-brand" href="#">
             <img src="@/assets/logo1.png" class="img-fluid" alt="R2B">
@@ -24,24 +24,24 @@
             <div class="offcanvas-body">
               <ul class="navbar-nav mx-auto mb-lg-0">
                 <li class="nav-item me-3">
-                  <router-link class="nav-link text-white" to="/">Home</router-link>
+                  <router-link class="nav-link" to="/">Home</router-link>
                 </li>
                 <li class="nav-item me-3">
-                  <router-link class="nav-link text-white" to="/about">About</router-link>
+                  <router-link class="nav-link" to="/about">About</router-link>
                   
                 </li>
                 <li class="nav-item me-3">
-                  <router-link class="nav-link text-white" to="/">Events</router-link>
+                  <router-link class="nav-link" to="/">Events</router-link>
                 </li>
                 <li class="nav-item me-3">
-                  <router-link class="nav-link text-white" to="/">Research and impact</router-link>
+                  <router-link class="nav-link" to="/">Research and impact</router-link>
                 </li>
               </ul>
 
               <div class="d-flex">
                 <ul class="navbar-nav mb-2 mb-lg-0">
                   <li class="nav-item me-3">
-                    <router-link class="nav-link text-white" to="/">Blog</router-link>
+                    <router-link class="nav-link" to="/">Blog</router-link>
                   </li>
                   <li class="nav-item">
                     <a href="#involved" class="btn s2b-text-bg-primary">Get involved</a>
