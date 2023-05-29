84cb44cc47ddfbaa7bd7ae7d164c734da7dc8eae
Shafi Akinropo <sakinropo@gmail.com>
2023-05-29 21:14:47 +0100

    update

--- src/components/About/AboutContent.vue
+++ src/components/About/AboutContent.vue
@@ -1,3 +1,28 @@
 <template>
-    Hello
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px; visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="img/about.jpg" alt="" style="object-fit: cover;">
+                    </div>
+                </div>
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <!-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> -->
+                    <h1 class="mb-4">Welcome to RikeSD</h1>
+                    <p class="mb-4">
+                        We are a not-for-profit organization dedicated to promoting sustainable development in communities across Africa. Our mission is to conduct impactful research, foster innovation, and empower entrepreneurs to drive positive change.
+                    </p>
+                    <p class="mb-4">
+                        At RIKE-SD, we believe in the power of the knowledge economy to transform societies and drive economic growth. Our initiatives are led by both private sector and not-for-profit organizations, working together to create a more prosperous and sustainable future for all.
+                    </p>
+                    <p class="mb-4">
+                        Join us in our mission to make a lasting impact on the African continent and beyond.
+                    </p>
+                    
+                    <a class="btn btn-sec py-3 px-5 mt-2" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
