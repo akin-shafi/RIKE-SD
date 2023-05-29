82552dc95465bc50bd36982500531ad7b617ead5
Shafi Akinropo <sakinropo@gmail.com>
2023-05-30 00:04:31 +0100

    update

--- src/components/About/AboutContent.vue
+++ src/components/About/AboutContent.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- Welcome Start -->
     <div class="container-xxl py-5">
         <div class="container">
             <div class="row g-5">
@@ -21,6 +22,71 @@
                     </p>
                     
                     <a class="btn btn-sec py-3 px-5 mt-2" href="">Read More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Welcome End -->
+
+
+    <!-- Mission & Objective start-->
+    <div class="container-xxl bg-app-primary">
+        <div class="container">
+            <div class="row d-flex align-items-center" style="min-height: 300px; ">
+                
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <!-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> -->
+                    <h1 class="mb-4">Our mission</h1>
+                    <p class="mb-4 text-justified">
+                        Achieving development impacts by building new knowledge-based economies in communities. 
+                        Through responsible and responsive utilization of research and innovation outcomes and outputs.
+                    </p>
+                    
+                </div>
+
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <h1 class="mb-4">Our Goal</h1>
+                    <p class="mb-4 text-justified">
+                        To fast-track sustainable development in Africa using STI & R4D 
+                        solutions for connecting and achieving global, continental, regional and national development agenda
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- M & O End -->
+
+
+    <div class="container-xxl py-5 ">
+        <div class="container">
+            <div class="row g-5">
+                
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
+                    <!-- <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6> -->
+                    <h1 class="mb-4">Objectives</h1>
+                    <p class="mb-4">
+                        <ul>
+                            <li class="pb-2 text-justified">To lead advocacy and capacity development for innovation systems and research-enabled impact investment</li>
+
+                            <li class="pb-2 text-justified">To promote, harness, and mainstream research results and evidence synthesis into policies and practices for state and non-state actors of the African economies</li>
+
+                            <li class="pb-2 text-justified">To raise, manage, and warehouse research and innovation funding from partners, collaborators, and funders globally</li>
+
+                            <li class="pb-2 text-justified">To support and promote research to impact activities at the graduate and postgraduate levels of tertiary institutions.</li>
+
+                            <li class="pb-2 text-justified">To catalyze the building and integration of national quality infrastructure into research practices through the establishment of R4D-led think tanks, communities of practice, and clusters of actors across the activity sectors of African economies.</li>
+
+                            <li class="pb-2 text-justified">To support the strengthening, tracking, and development of the national STI ecosystem for impacts, research excellence, global visibility of researchers and their research activities, enhanced institutional ranking, and national competitiveness</li>
+                        </ul>
+                    </p>
+                    
+                </div>
+
+                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px; visibility: visible; animation-delay: 0.1s; animation-name: fadeInUp;">
+                    <div class="position-relative h-100">
+                        <img class="img-fluid position-absolute w-100 h-100" src="@/assets/amenities.avif" alt="" style="object-fit: cover;">
+                    </div>
                 </div>
             </div>
         </div>
